bd0d7f6235ab2ba381a0bf452c7828be510b6daf
Механошин Алексей <a.mekhanoshin@dssl.ru>
2024-12-17 14:08:02 +0300

    linux build script fixes

--- build-debian64.sh
+++ build-debian64.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+rm -rf build
 mkdir -p build
 conan install conan/debian-64/conanfile.txt --profile conan/debian-64/conanprofile.debug --output-folder=. --build=missing
 conan install conan/debian-64/conanfile.txt --profile conan/debian-64/conanprofile.release --output-folder=. --build=missing
 pushd build
 
-echo "PWD=[$(pwd)]"
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$(pwd)/Release/generators/conan_toolchain.cmake  -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
 cmake --build . --config Release
 popd
