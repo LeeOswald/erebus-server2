43736bf2246378e63319b3e36c51bd9e286e5fd3
Механошин Алексей <a.mekhanoshin@dssl.ru>
2025-01-05 16:22:17 +0300

    obsolete

--- src/system/logger/logger2.cxx
+++ src/system/logger/logger2.cxx
@@ -1,9 +1,37 @@
-#include <erebus/system/logger2.hxx>
+#include <erebus/system/logger/null_logger2.hxx>
 
 
-namespace Er::Log2
+namespace Er
+{
+    
+namespace
 {
 
+Er::Log2::ILogger* nullLogger() noexcept
+{
+    static Er::Log2::NullLogger dummy;
+    return &dummy;
+}
 
+thread_local Er::Log2::ILogger* g_logger = nullLogger();
+    
+} // namespace {}
 
-} // namespace Er::Log2 {}
+namespace Log2
+{
+
+ER_SYSTEM_EXPORT Log2::ILogger* get() noexcept
+{
+    return g_logger;
+}
+
+ER_SYSTEM_EXPORT Log2::ILogger* set(Log2::ILogger* log) noexcept
+{
+    auto prev = g_logger;
+    g_logger = log ? log : nullLogger();
+    return prev;
+}
+
+} // namespace Log2 {}
+
+} // namespace Er {}
