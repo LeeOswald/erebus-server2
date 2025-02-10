f0bc9ca95e9604dd13002091bd0132ff36a41a51
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2025-02-10 11:54:55 +0300

    TestEventListener impl

--- src/system/tests/main.cpp
+++ src/system/tests/main.cpp
@@ -16,40 +16,173 @@ namespace
 class TestEventListener 
     : public testing::EmptyTestEventListener 
 {
+    void OnTestIterationStart(const testing::UnitTest& test, int iteration) override
+    {
+        Er::Log2::info(Er::Log2::get(), "[==========] Running {} from {}.", test.test_to_run_count(), test.test_suite_to_run_count());
+    }
+
+    void OnTestIterationEnd(const testing::UnitTest& test, int iteration) override
+    {
+        Er::Log2::info(Er::Log2::get(), "[==========] {} from {} ran ({} ms total).", test.test_to_run_count(), test.test_suite_to_run_count(), test.elapsed_time());
+
+        Er::Log2::info(Er::Log2::get(), "[  PASSED  ] {}", test.successful_test_count());
+
+        auto skipped_test_count = test.skipped_test_count();
+        if (skipped_test_count > 0) 
+        {
+            Er::Log2::info(Er::Log2::get(), "[  SKIPPED ] {}", skipped_test_count);
+            for (int i = 0; i < test.total_test_suite_count(); ++i) 
+            {
+                auto& test_suite = *test.GetTestSuite(i);
+                if (!test_suite.should_run() || (test_suite.skipped_test_count() == 0)) 
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < test_suite.total_test_count(); ++j) 
+                {
+                    auto& test_info = *test_suite.GetTestInfo(j);
+                    if (!test_info.should_run() || !test_info.result()->Skipped()) 
+                    {
+                        continue;
+                    }
+                    Er::Log2::info(Er::Log2::get(), "[  SKIPPED ] {}.{}", test_suite.name(), test_info.name());
+                }
+            }
+        }
+
+        if (!test.Passed()) 
+        {
+            auto failed_test_count = test.failed_test_count();
+            Er::Log2::info(Er::Log2::get(), "[  FAILED  ] {}", failed_test_count);
+
+            for (int i = 0; i < test.total_test_suite_count(); ++i) 
+            {
+                auto& test_suite = *test.GetTestSuite(i);
+                if (!test_suite.should_run() || (test_suite.failed_test_count() == 0)) 
+                {
+                    continue;
+                }
+
+                for (int j = 0; j < test_suite.total_test_count(); ++j) 
+                {
+                    auto& test_info = *test_suite.GetTestInfo(j);
+                    if (!test_info.should_run() || !test_info.result()->Failed()) 
+                    {
+                        continue;
+                    }
+                    Er::Log2::info(Er::Log2::get(), "[  FAILED  ] {}.{}", test_suite.name(), test_info.name());
+                    printf("%s.%s", test_suite.name(), test_info.name());
+
+                    auto type_param = test_info.type_param();
+                    auto value_param = test_info.value_param();
+
+                    if (type_param)
+                    {
+                        Er::Log2::info(Er::Log2::get(), "TypeParam = {}", type_param);
+                    }
+
+                    if (value_param)
+                    {
+                        Er::Log2::info(Er::Log2::get(), "GetParam() = {}", value_param);
+                    }
+                }
+            }
+            Er::Log2::info(Er::Log2::get(), "{} FAILED TESTS", failed_test_count);
+        }
+
+        auto num_disabled = test.reportable_disabled_test_count();
+        if (num_disabled) 
+        {
+            if (test.Passed()) 
+            {
+                Er::Log2::info(Er::Log2::get(), "");  // Add a spacer if no FAILURE banner is displayed.
+            }
+
+            Er::Log2::info(Er::Log2::get(), "  YOU HAVE {} DISABLED TESTS\n", num_disabled);
+        }
+    }
+
+    void OnEnvironmentsSetUpStart(const testing::UnitTest&) override
+    {
+        Er::Log2::info(Er::Log2::get(), "[----------] Global test environment set-up.");
+    }
+
+    void OnEnvironmentsTearDownStart(const testing::UnitTest&) override
+    {
+        Er::Log2::info(Er::Log2::get(), "[----------] Global test environment tear-down");
+    }
+
     void OnTestSuiteStart(const testing::TestSuite& suite) override 
     {
-        Er::Log2::info(Er::Log2::get(), "[{}] ------------------", suite.name());
+        Er::Log2::info(Er::Log2::get(), "[----------] {} from {}", suite.test_to_run_count(), suite.name());
     }
 
     void OnTestSuiteEnd(const testing::TestSuite& suite) override
     {
+        Er::Log2::info(Er::Log2::get(), "[----------] {} from {} ({} ms total)\n", suite.test_to_run_count(), suite.name(), suite.elapsed_time());
     }
 
     void OnTestStart(const testing::TestInfo& test) override 
     {
-        m_failed = 0;
-        Er::Log2::info(Er::Log2::get(), "[RUN {}]", test.name());
+        Er::Log2::info(Er::Log2::get(), "[ RUN      ] {}.{}", test.test_suite_name(), test.name());
+    }
+
+    void OnTestDisabled(const testing::TestInfo& test) override
+    {
+        Er::Log2::info(Er::Log2::get(), "[ DISABLED ] {}.{}", test.test_suite_name(), test.name());
     }
 
     void OnTestPartResult(const testing::TestPartResult& result) override 
     {
-        if (result.failed())
+        switch (result.type()) 
         {
-            ++m_failed;
-            Er::Log2::info(Er::Log2::get(), "[    FAILED] in {}:{}\n{}", result.file_name(), result.line_number(), result.summary());
+            // If the test part succeeded, we don't need to do anything.
+        case testing::TestPartResult::kSuccess:
+            return;
+        
+        case testing::TestPartResult::kSkip:
+            Er::Log2::info(Er::Log2::get(), "{}({}) Skipped", result.file_name(), result.line_number());
+
+        case testing::TestPartResult::kNonFatalFailure:
+        case testing::TestPartResult::kFatalFailure:
+            Er::Log2::info(Er::Log2::get(), "{}({}) Error", result.file_name(), result.line_number());
+            if (result.message() && *result.message())
+                Er::Log2::info(Er::Log2::get(), "{}", result.message());
         }
     }
     
     void OnTestEnd(const testing::TestInfo& test) override 
     {
-        if (!m_failed)
+        if (test.result()->Passed()) 
         {
-            Er::Log2::info(Er::Log2::get(), "[    OK]");
+            Er::Log2::info(Er::Log2::get(), "[       OK ] {}.{} ({} ms)", test.test_suite_name(), test.name(), test.result()->elapsed_time());
+        }
+        else if (test.result()->Skipped()) 
+        {
+            Er::Log2::info(Er::Log2::get(), "[  SKIPPED ] {}.{} ({} ms)", test.test_suite_name(), test.name(), test.result()->elapsed_time());
+        }
+        else 
+        {
+            Er::Log2::info(Er::Log2::get(), "[  FAILED  ] {}.{} ({} ms)", test.test_suite_name(), test.name(), test.result()->elapsed_time());
+        }
+        
+        if (test.result()->Failed())
+        {
+            auto type_param = test.type_param();
+            auto value_param = test.value_param();
+
+            if (type_param) 
+            {
+                Er::Log2::info(Er::Log2::get(), "TypeParam = {}", type_param);
+            }
+
+            if (value_param) 
+            {
+                Er::Log2::info(Er::Log2::get(), "GetParam() = {}", value_param);
+            }
         }
     }
-
-private:
-    unsigned m_failed = 0;
 };
 
 class TestApplication final
@@ -123,11 +256,11 @@ private:
         }
 #endif
         ::testing::InitGoogleTest(&argc, argv);
-
+#if 1
         auto& listeners = testing::UnitTest::GetInstance()->listeners();
         delete listeners.Release(listeners.default_result_printer());
         listeners.Append(new TestEventListener);
-
+#endif
         return RUN_ALL_TESTS();
     }
 };
