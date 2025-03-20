ce4fe99c127cb3cba9868a6666b2416e8c04f7bd
Aleksey Mekhanoshin <aleksey.mekhanoshin@gmail.com>
2025-03-20 11:48:13 +0300

    built on windows

--- build-windows64.cmd
+++ build-windows64.cmd
@@ -1,8 +1,5 @@
-set VSCMD_DEBUG=3
 mkdir build
-conan install conan\win64-vc143\conanfile.txt --profile conan\win64-vc143\conanprofile.debug --output-folder=. --build=missing
-conan install conan\win64-vc143\conanfile.txt --profile conan\win64-vc143\conanprofile.release --output-folder=. --build=missing
 pushd build
-cmake .. -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=generators\conan_toolchain.cmake  -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
-rem cmake --build . --config Release
+cmake .. -G "Visual Studio 17 2022" -DCMAKE_TOOLCHAIN_FILE=generators\conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
+cmake --build . --config Release
 popd
---
+++ update-conan.cmd
@@ -0,0 +1,2 @@
+conan install conan\win64-vc143\conanfile.txt --profile conan\win64-vc143\conanprofile.debug --output-folder=. --build=missing
+conan install conan\win64-vc143\conanfile.txt --profile conan\win64-vc143\conanprofile.release --output-folder=. --build=missing
